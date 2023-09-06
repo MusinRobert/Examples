922d1ee2928637298bba914b4c15fa8808b020db
Robert <otdelniiadres@gmail.com>
2023-09-06 21:51:08 +0500

    add Example 010

---
+++ Example008/Program.cs
@@ -0,0 +1,25 @@
+﻿int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int count = 0;
+int distance = 10000;
+int friend = 2;
+int time = 0;
+
+while (distance > 10)
+{
+    if (friend == 2)
+    {
+        time = distance / (dogSpeed + secondFriendSpeed);
+        friend = 1;
+    }
+    else
+    {
+        time = distance / (dogSpeed + firstFriendSpeed);
+        friend = 2;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count++;
+}
+
+Console.WriteLine("Собака пробежит " + count + " раз");
